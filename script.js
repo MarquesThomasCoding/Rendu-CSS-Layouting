b45b4f0dcaf3abbcd88e7d175773b432e2ad076c
Thomas Marques <thomas.marques@edu.devinci.fr>
2023-10-22 15:18:03 +0200

    🔨 UPDATE script

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const burgerMenu = document.querySelector('.menu-burger > ul');
 
 burger.addEventListener('click', () => {
     // Si le menu a un style "display" en block, on le cache
-    if (burgerMenu.style.display === 'block') {
+    if (burgerMenu.style.display === 'flex') {
         burgerMenu.style.display = 'none';
     } else {
         // Sinon, on affiche le menu
-        burgerMenu.style.display = 'block';
+        burgerMenu.style.display = 'flex';
     }
 });
